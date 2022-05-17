32855234de248f94775f48a2256ac7fc8b3545ec
Witold Matusiński <matusinski@interia.pl>
2022-05-17 12:14:14 +0200

    Inicjalizacja projektu //wersja 2

---
+++ src/main/java/pl/wszib/pizzamarket2/Pizzamarket2Application.java
@@ -0,0 +1,13 @@
+package pl.wszib.pizzamarket2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pizzamarket2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pizzamarket2Application.class, args);
+	}
+
+}
---
+++ src/test/java/pl/wszib/pizzamarket2/Pizzamarket2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wszib.pizzamarket2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pizzamarket2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
